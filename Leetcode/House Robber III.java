1cd35d84d83f8ca3711f3e05a4e7b15089fd0d35
Henson Zhong <939203458@qq.com>
2017-07-29 00:32:27 +0800

    更新house robber III

--- Leetcode/House Robber III.java
+++ Leetcode/House Robber III.java
@@ -9,7 +9,9 @@
  */
 public class Solution {
     public int rob(TreeNode root) {
-        int[] res = dfs(root);
+        // 每个节点有两种状态：不抢该节点，抢该节点
+        // res[0]表示抢该节点时，所获取的最大金额；res[1]表示不抢该节点时，所获取的最大金额
+        int[] res = dfs(root);  // 深度优先搜索计算各个节点的res值
         return Math.max(res[0], res[1]);
     }
 
