2dc49867fc949b316c8669e4f8ec5668e1537f9b
Henson Zhong <939203458@qq.com>
2017-08-28 17:26:02 +0800

    update

--- Leetcode/Subarray Sum Equals K.java
+++ Leetcode/Subarray Sum Equals K.java
@@ -1,13 +1,14 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int sum = 0, res = 0;
+        int sum = 0, res = 0;  // sum表示累加和
         
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);  // k!=0时，如果sum=k，为了考虑到0~i这一段，需要put<0,1>
+        map.put(0, 1);  // 如果sum=k的话，即0~当前位置的这段子数组的和为k，put(0,1)是为了表示这个结果
         
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            res += map.getOrDefault(sum - k, 0);  // 颠倒的话，k=0的结果会多1
+            res += map.getOrDefault(sum - k, 0);  // 在当前位置之前，有多少段子数组的和为sum-k
+            
             map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         
