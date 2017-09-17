b37ab26bd3728bca4fcdfd7dee7a57999f1a2edc
Henson Zhong <939203458@qq.com>
2017-09-17 22:31:08 +0800

    update

--- Leetcode/Reverse Linked List.java
+++ Leetcode/Reverse Linked List.java
@@ -9,35 +9,17 @@
 public class Solution {
     // 遍历时直接改变方向
     public ListNode reverseList(ListNode head) {
-        ListNode prev = null, next = null;
-        while (head != null) {
-            next = head.next;
-            head.next = prev;
-            prev = head;
-            head = next;
-        }
-        return prev;
-    }
-
-    /*
-    // 前插法
-    public ListNode reverseList(ListNode head) {
         if (head == null) return head;
+        
+        // 创建三个指针
+        ListNode pre = null, node = head, next = null;
 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-
-        ListNode pre = dummy;
-        ListNode start = pre.next;
-        ListNode next = start.next;
-
-        while (next != null) {
-            start.next = next.next;
-            next.next = pre.next;
-            pre.next = next;
-            next = start.next;
+        while (node != null) {
+            next = node.next;
+            node.next = pre;
+            pre = node;
+            node = next;
         }
-        return dummy.next;
+        return pre;
     }
-    */
 }
---
+++ Leetcode/Reverse Nodes in k-Group.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode cur = head;
+        int count = 0;
+        while (cur != null && count < k) {
+            cur = cur.next;
+            count++;
+        }
+        
+        if (count == k) {
+            cur = reverseKGroup(cur, k);
+
+            // cur表示已反转部分的头结点
+            // head表示待反转部分的头结点
+            while (count-- > 0) {
+                ListNode temp = head.next;
+                head.next = cur;
+                cur = head;
+                head = temp;
+            }
+            head = cur;
+        }
+        
+        return head;
+    }
+}
