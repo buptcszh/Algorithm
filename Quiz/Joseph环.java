8b0d8caefd99e136f3b00bc77956108143c9ccf4
Henson Zhong <939203458@qq.com>
2017-09-28 14:52:47 +0800

    update

--- Quiz/Joseph环.java
+++ Quiz/Joseph环.java
@@ -1,16 +1,13 @@
 public class Joseph {
-
+    // 求圆圈中最后剩下的数字
     public static void main(String[] args) {
-
         System.out.println(Joseph.find(5, 2));
     }
 
-    public int find(int n, int m) {
-
+    public static int find(int n, int m) {
         // 假设编号从0开始
         int res = 0;
         for (int i = 2; i <= n; i++) {
-
             // f[i]表示剩下i个人时，最后留下的那个人的编号
             // 递推关系为f[i] = (f[i-1] + m) % i
             res = (res + m) % i;
@@ -19,3 +16,10 @@ public class Joseph {
         return res;
     }
 }
+
+// n = 7, m = 3
+// 0 1 2 3 4 5 6
+//
+// 3 4 5 6 0 1
+// 0 1 2 3 4 5 重新编号
+// origin = (cur + m) mod n;
